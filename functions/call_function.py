01e275cf0e3b464d4880c5701977a78b901e9e7b
Rudy <duyr190@gmail.com>
2025-10-13 12:33:19 -0400

    calling setup

--- functions/call_function.py
+++ functions/call_function.py
@@ -10,4 +10,28 @@ def call_function(function_call_part: types.FunctionCall, verbose=False):
     except Exception as error:
         print(f"Error: {error} fail calling function")
 
-    return ""
+    # attempt to run function pass the result down
+    try:
+        # map function from name
+        function_name = function_call_part.name
+        function_result = ""
+    except Exception as error:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
